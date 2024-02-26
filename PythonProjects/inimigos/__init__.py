0a05c23bd624389b452c154d088f97a47fcc5c08
Lucas Oliviera Corado <153236868+Corado425@users.noreply.github.com>
2024-02-26 17:03:24 -0300

    Update __init__.py
    New enemies!

--- PythonProjects/inimigos/__init__.py
+++ PythonProjects/inimigos/__init__.py
@@ -1,31 +1,30 @@
-from random import randint, choice
+from random import randint
 from time import sleep
 
 
-class Goblin:
-    nomes_goblin = ['\033[31mStradald\033[m', '\033[31mCrokokt\033[m', '\033[31mBregonk\033[m', '\033[31mZraald\033[m',
-                    '\033[31mXiglegz\033[m', '\033[31mUbhigs\033[m', '\033[31mDring\033[m', '\033[31mStreec\033[m'
-                    , '\033[31mPrur\033[m', '\033[31mDragdeat\033[m', '\033[31mCaamoikx\033[m', '\033[31mKraax\033[m']
-
-    def __init__(self, vivo=True):
-        self.nome = choice(self.nomes_goblin)
-        self.nomes_goblin.remove(self.nome)  # Feito para não repetir os nomes
+class Inimigo:
+    def __init__(self, tipo, habilidade, vivo=True):
         self.vivo = vivo
         self.poder = randint(5, 10)
-        self.vida = randint(35, 40)
+        self.vida = randint(30, 35)
+        self.habilidade = f'\033[35m{habilidade}\033[m'
+        self.tipo = f'\033[31m{tipo}\033[m'
+
+    def ultimate(self, protagonista):
+        return self.habilidade
 
     def atacar(self, protagonista):
 
         print('-=' * 50)
         sleep(2)
-        print(f'Vez de {self.nome} atacar!')
+        print(f'Vez do {self.tipo} atacar!')
         sleep(2)
         d20 = randint(1, 20)
         sleep(2)
 
         if 1 <= d20 <= 5:
 
-            print(f'\n{self.nome} errou o ataque!')
+            print(f'\n{self.tipo} errou o ataque!')
             sleep(2)
             print(f'\n{protagonista.nome} ativou \033[35mATAQUE DE OPORTUNIDADE\033[m')
             sleep(2)
@@ -34,41 +33,90 @@ class Goblin:
             print('Rolando dado...')
             dano = randint(1, 6)
             sleep(2)
-            print(f'\nVocê causou \033[33m{dano} de dano\033[m no HP de {self.nome}')
+            print(f'\nVocê causou \033[33m{dano} de dano\033[m no HP do {self.tipo}')
             self.vida -= dano
             if self.vida <= 0:
                 self.vivo = False
 
         elif 6 <= d20 <= 9:
             dano = self.poder * 0.5
-            print(f'\n{self.nome} acertou um ataque fraco! {protagonista.nome} recebeu \033[33m{dano:.2f} de dano\033[m'
-                  f' físico')
+            print(f'\nO {self.tipo} acertou um ataque fraco! {protagonista.nome} recebeu \033[33m{dano:.2f} '
+                  f'de dano\033[m físico')
             protagonista.vida_atual -= dano
 
         elif 10 <= d20 <= 17:
             dano = self.poder
-            print(f'\n{self.nome} acertou {protagonista.nome} em cheio, causando \033[33m{dano:.2f} de dano\033[m '
+            print(f'\nO {self.tipo} acertou {protagonista.nome} em cheio, causando \033[33m{dano:.2f} de dano\033[m '
                   f'físico!')
             protagonista.vida_atual -= dano
 
         else:
-            dano = self.poder * 1.5
-            print(f'\n{self.nome} ativou a habilidade \033[35mDILACERAR\033[m que causa 50% a mais de dano!')
-            print(f'\n{protagonista.nome} sofreu \033[33m{dano:.2f} de dano\033[m físico!')
-            protagonista.vida_atual -= dano
+            self.ultimate(protagonista)
 
         if protagonista.vida_atual > 0:
             print('-=' * 50)
             sleep(2)
             print(f'\nVida atual de {protagonista.nome}: {protagonista.vida_atual:.2f} HP')
-            print(f'Vida atual de {self.nome}: {self.vida:.2f} HP\n')
+            print(f'Vida atual do {self.tipo}: {self.vida:.2f} HP\n')
 
         else:
             print('-=' * 50)
             sleep(2)
-            print(f'\nVocê foi morto por {self.nome}!\n')
+            print(f'\nVocê foi morto pelo {self.tipo}!\n')
             protagonista.vivo = False
             print('-=' * 50)
 
     def info(self, prota):
-        return print(f'{prota.nome}, agora você enfrentará {self.nome}, um goblin com {self.poder} FC e {self.vida} HP')
+        return print(f'{prota.nome}, você enfrentará um {self.tipo} com {self.poder} FC e {self.vida} HP')
+
+
+class Goblin(Inimigo):
+    def __init__(self, tipo='Goblin', habilidade='DILACERAR', vivo=True):
+        super().__init__(tipo, habilidade, vivo)
+
+    def ultimate(self, protagonista):
+        d6 = (randint(1, 6) / 10) + 1
+        dano = self.poder * d6
+        print(f'\nO {self.tipo} ativou a habilidade \033[35m{self.habilidade}\033[m!')
+        sleep(2)
+        print(f'\nUm d6 será rolado para determinar o dano adicional')
+        sleep(2)
+        print(f'\nO {self.tipo} usou uma pequena faca para cortar o corpo de {protagonista.nome}, causando {dano}'
+              f' de dano!')
+        protagonista.vida_atual -= dano
+
+
+class Orc(Inimigo):
+    def __init__(self, tipo='Orc', habilidade='ARREMESSO DE PEDRA', vivo=True):
+        super().__init__(tipo, habilidade, vivo)
+        self.vida = randint(35, 45)
+        self.poder = randint(10, 15)
+
+    def ultimate(self, protagonista):
+        d6 = (randint(1, 6) / 10) + 1
+        dano = self.poder * d6
+        print(f'\nO {self.tipo} ativou a habilidade \033[35m{self.habilidade}\033[35m!')
+        sleep(2)
+        print(f'\nUm d6 será rolado para determinar o dano adicional')
+        sleep(2)
+        print(f'\nO {self.tipo} arrancou uma grande rocha fincada na parede, arremessando-a em {protagonista.nome}!'
+              f' {dano} de dano foi causado!')
+        protagonista.vida_atual -= dano
+
+
+class Troll(Inimigo):
+    def __init__(self, tipo='Troll', habilidade='MORDIDA', vivo=True):
+        super().__init__(tipo, habilidade, vivo)
+        self.vida = randint(45, 60)
+        self.poder = randint(15, 20)
+
+    def ultimate(self, protagonista):
+        d6 = (randint(1, 6) / 10) + 1
+        dano = self.poder * d6
+        print(f'\nO {self.tipo} ativou a habilidade \033[35m{self.habilidade}\033[35m!')
+        sleep(2)
+        print(f'\nUm d6 será rolado para determinar o dano adicional')
+        sleep(2)
+        print(f'\nO {self.tipo} mordeu o corpo de {protagonista.nome} com suas presas afiadas, '
+              f'arrancando um pedaço de sua carne! {dano} de dano foi causado!')
+        protagonista.vida_atual -= dano
