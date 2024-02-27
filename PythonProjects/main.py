9af6595a5aaa62ca9885a234f428e4fb8014f342
Lucas Oliviera Corado <153236868+Corado425@users.noreply.github.com>
2024-02-27 12:14:12 -0300

    Update main.py

--- PythonProjects/main.py
+++ PythonProjects/main.py
@@ -3,10 +3,11 @@ from protagonista import *
 
 
 def dungeon(player, covil):
+    print(f'\nVocê adentrou no covil dos {covil[0].tipo}\033[31ms\033[m\n')
     for inimigo in covil:
 
         if player.vivo:
-            inimigo.info(player)
+            inimigo.info()
 
         while inimigo.vivo and player.vivo:
 
@@ -23,6 +24,7 @@ def dungeon(player, covil):
                     break
 
     if player.vivo:
+        print('-=' * 50)
         print(f'\nVocê completou a dungeon\n')
         player.level_up()
 
@@ -32,8 +34,9 @@ def dungeon(player, covil):
 
 jogador = Protagonista(input('Qual o seu nome, guerreiro? '))
 jogador.nome = f'\033[034m{jogador.nome}\033[m'
+jogador.info()
 
-covil_goblins = [Goblin() for x in range(3)]
+covil_goblins = [Goblin() for x in range(1)]
 covil_orcs = [Orc() for y in range(2)]
 covil_trolls = [Troll() for z in range(2)]
 
